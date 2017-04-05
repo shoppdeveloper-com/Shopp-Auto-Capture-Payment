bb29ce24581fba12848f0e04635c7f3aabbfc2e3
Shoppdeveloper.com Team <support@shoppdeveloper.com>
2017-04-05 19:32:32 +0200

    Update sacp.php

--- sacp.php
+++ sacp.php
@@ -3,34 +3,35 @@
 * Plugin Name: Shopp Auto Capture Payment
 * Plugin URI: http://www.shoppdeveloper.com/shopp-auto-capture-payment
 * Description: Add-on for Shopp e-commerce plugin to auto capture payments.
-* Author: Shoppdeveloper.com Team
+* Author: Shoppdeveloper.com
 * Author URI: http://www.shoppdeveloper.com
-* Version: 0.1.2
+* Version: 0.1.3
 */
 
 defined( 'WPINC' ) || header( 'HTTP/1.1 403' ) & exit;
 
-add_action('init','sacp_init');
+add_action('init', 'sacp_init');
 
-function sacp_init(){
+function sacp_init() {
 
     // only start if Shopp is activated
     if ( function_exists('shopp') ) {
 
 		// exclude paymentgateways that have no capture option
-		$exclude = array('ShoppOfflinePayment');
+		$exclude = array('ShoppOfflinePayment' => 'ShoppOfflinePayment');
 		// retrieve all active gateways from database
 		$active = shopp_setting('active_gateways');
 		// if any active gateways present
 		if ( ! empty($active) ) {
 			// convert string to array
-			$gateways = explode(',',$active);
+			$gateways = explode(',', $active);
 			// loop through available gateways
-			foreach ( $gateways as $key=>$value ){
+			foreach ( $gateways as $key => $value ) {
 				// remove counter number from gateway name
 				$gateway = split('-', $value);
 				// if gateway name is not present in $exclude, apply the auto-capture filter
-				if ( ! in_array($gateway[0], $exclude) ) add_filter('shopp_purchase_order_' . strtolower($gateway[0]) . '_processing', create_function('', 'return "sale";'));
+				if ( ! isset($exclude[ $gateway[0] ] ) )
+					add_filter('shopp_purchase_order_' . strtolower($gateway[0]) . '_processing', create_function('', 'return "sale";'));
 			}
 		}
     }
